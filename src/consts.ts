5617b6a441361cc34feac42a22d09c3246900947
oscaramontiel <etherealflux94@gmail.com>
2023-10-16 10:41:33 -0600

    hotfix(consts.ts): Changes site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Óscar A. Montiel";
-export const SITE_DESCRIPTION =
-  "Creative Software Engineer and Artist. Experienced React Developer specializing in the ReactThreeFiber ecosystem for dynamic 3D web experiences. Expertise in Jest Testing and Storybook UI for robust, scalable applications. Adept at ChatGPT and utilizing AWS Services for cloud-based solutions. Skilled in Three.js Graphics for interactive web content and art. Proficient in Git Version Control, Yarn Package Manager, and NPM Modules to maintain codebase integrity. Highly experienced in Jira Project Management for streamlined workflows. Capable of creating highly optimized websites through SEO Optimization, Gatsby Static Sites, and Webflow Designer capabilities.";
+export const SITE_DESCRIPTION = "Creative Software Engineer";
 export const TWITTER_PROFILE = "https://twitter.com/geosminu";
 export const GITHUB_PROFILE = "https://github.com/animanoir";
