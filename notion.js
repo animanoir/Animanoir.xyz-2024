33e7d352607126086eee3d5c74057b4c5e91e6f6
oscaramontiel <etherealflux94@gmail.com>
2024-09-23 23:05:42 -0600

    chore: Update Astro config, remove "quality" attribute from image elements, update CV file name and content, and add prefetching

--- notion.js
+++ notion.js
@@ -7,7 +7,13 @@ const notion = new Client({
 export async function getDatabase(databaseId){
   const response = await notion.databases.query({
     database_id: databaseId,
-    page_size: 13
+    page_size: 13,
+    sorts: [
+      {
+        timestamp: 'last_edited_time',
+        direction: 'descending'
+      }
+    ]
   })
   return response.results
 }
