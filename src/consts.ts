99aa97a7637c0612ba05b5e7a42579751eef4ff8
oscaramontiel <etherealflux94@gmail.com>
2024-12-27 20:44:17 -0600

    Update site description for clarity and consistency across layouts

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,4 @@
 
 export const SITE_TITLE = "Óscar A. Montiel";
 export const SITE_SUBTITLE = "Multidisciplinary Media Artist";
-export const SITE_DESCRIPTION =
-  "Software Engineer by profession—Multidisciplinary Media Artist by choice. Most of the time everything & nothing at once. Just like a poet, I tend to rhyme without knowing. Unleashed and boundless, just like you!";
+export const SITE_DESCRIPTION = "Multidisplinary Media Artist—A/visuals & Interactive Art.";
