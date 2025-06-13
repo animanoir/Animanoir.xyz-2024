3dcd313020ee90bace64039a352e0597494baf87
oscaramontiel <etherealflux94@gmail.com>
2025-06-12 22:16:15 -0600

    starting changes

---
+++ src/components/RandomVideo/GeneralRandomVideo.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import styles from "./RandomVideo.module.css";
+
+export default function GeneralRandomVideo({ videoPath }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [videoSrc, setVideoSrc] = useState(videoPath)
+  
+  const handleLoadedData = () => {
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setVideoSrc(videoPath)
+  },[])
+
+  return (
+    <div className={styles.videoContainer}>
+      {isLoading && (
+        <div className={styles.loaderContainer}>
+          <div className={styles.loader}></div>
+        </div>
+      )}
+      <video
+        key={videoSrc}
+        autoPlay
+        muted
+        loop
+        playsInline
+        onLoadedData={handleLoadedData}
+        className={`${styles.video} ${isLoading ? styles.hidden : styles.visible}`}
+      >
+        <source src={videoPath} type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
