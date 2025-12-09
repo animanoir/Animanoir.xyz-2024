1048d10b3d93468751476753201b8899bc417eae
oscaramontiel <etherealflux94@gmail.com>
2025-12-08 23:31:03 -0600

    config: enable Sanity CDN

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,7 +5,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET || "production",
-  useCdn: false,
+  useCdn: true,
   apiVersion: "2024-01-01",
 });
 
