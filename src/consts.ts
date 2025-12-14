474a52f1f54e339df1ae8d3b8a0c5138df46bbcd
oscaramontiel <etherealflux94@gmail.com>
2025-12-14 14:25:37 -0600

    feat: update site subtitle and description.

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Óscar A. Montiel";
-export const SITE_SUBTITLE = "Interactive Media Developer";
-export const SITE_DESCRIPTION = "Software engineer specialized in web development, game programming & 3D/multimedia interactive art.";
+export const SITE_SUBTITLE = "Interactive Media Artist";
+export const SITE_DESCRIPTION = "Creative Developer & Interactive Media Artist, specialized in web development, game programming and 3D animation.";
